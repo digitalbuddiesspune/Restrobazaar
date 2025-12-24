8955748d4945a655d83416735a46fb339832ffc7
Prajwal Kamdi Gamotech <digitalbuddiesspune@gmail.com>
2025-12-24 16:07:54 +0530

    added sign-in sin-up feature

---
+++ backend/controller/signInController.js
@@ -0,0 +1,23 @@
+import User from '../models/user.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+const signIn = async (req, res) => {
+    try{
+        const {email, password}= req.body;
+        const user = await User.findOne({email});
+        if(!user){
+            return res.status(401).json({message: 'user not found'});
+        }
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if(!isPasswordCorrect){
+            return res.status(401).json({message: 'Invalid password'});
+        }
+        const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET, {expiresIn: '1d'});
+        res.status(200).json({message: 'Login successful', token});
+    }catch(error){
+        console.error(error);
+        res.status(500).json({message: 'Login failed'});
+    }
+}
+export default signIn;
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,24 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+import cors from 'cors';
+import connectDB from './config/databaseConnection.js';
+import signInRouter from './routes/signInRoute.js';
+import signUpRouter from './routes/signUpRoute.js';
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
+import conectDB from './config/databaseConnection.js';
+connectDB();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use("/api/v1", signInRouter);
+app.use("/api/v1", signUpRouter);
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
-
