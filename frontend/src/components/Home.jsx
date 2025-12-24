13cca52e1821b10a2d62fcc7d31d4a532f3c1846
Prajwal Kamdi Gamotech <digitalbuddiesspune@gmail.com>
2025-12-24 13:11:06 +0530

    Setup the backend

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import HeroSlider from './HeroSlider';
+import React from "react";
+import HeroSlider from "./HeroSlider";
 
 const Home = () => {
   return (
@@ -7,18 +7,23 @@ const Home = () => {
       {/* Hero Slider */}
       <HeroSlider
         slides={[
-          {
-            desktop: 'https://res.cloudinary.com/debhhnzgh/image/upload/v1765977603/Red_And_Orange_Elegant_Collection_Launch_Banner_1920_x_600_mm_3_hlv0eh.svg',
-            alt: 'TickNTrack - Premium Shoes & Watches Collection',
+           {
+            desktop:
+              "https://res.cloudinary.com/debhhnzgh/image/upload/v1766486888/Artboard_1_4x-100_ns3ryn.jpg",
+            alt: "Festive Offer - TickNTrack",
           },
           {
-            desktop: 'https://res.cloudinary.com/debhhnzgh/image/upload/v1766408022/33772651-2581-4408-bd07-e03a3741673c.png',
-            alt: 'TickNTrack - Premium Collection',
+            desktop:
+              "https://res.cloudinary.com/debhhnzgh/image/upload/v1765977603/Red_And_Orange_Elegant_Collection_Launch_Banner_1920_x_600_mm_3_hlv0eh.svg",
+            alt: "TickNTrack - Premium Shoes & Watches Collection",
           },
           {
-            desktop: 'https://res.cloudinary.com/debhhnzgh/image/upload/v1766126874/RestoBazaar-all_products_npuqtv.png',
-            alt: 'Festive Offer - TickNTrack',
+            desktop:
+              "https://res.cloudinary.com/debhhnzgh/image/upload/v1766408022/33772651-2581-4408-bd07-e03a3741673c.png",
+            alt: "TickNTrack - Premium Collection",
           },
+        
+         
         ]}
         mobileSrc="https://res.cloudinary.com/debhhnzgh/image/upload/v1765977544/Red_And_Orange_Elegant_Collection_Launch_Banner_1920_x_600_mm_1080_x_1080_px_1_zbleyf.svg"
       />
@@ -27,4 +32,3 @@ const Home = () => {
 };
 
 export default Home;
-
