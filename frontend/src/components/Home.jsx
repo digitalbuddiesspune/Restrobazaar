470e3b1c70ad480e597b70e83d063aeca9b4c38d
Rishikesh5577 <rishiwakchaure2003@gmail.com>
2025-12-25 16:12:01 +0530

    banner

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
         
          
         ]}
-        mobileSrc="https://res.cloudinary.com/debhhnzgh/image/upload/v1765977544/Red_And_Orange_Elegant_Collection_Launch_Banner_1920_x_600_mm_1080_x_1080_px_1_zbleyf.svg"
+        mobileSrc="https://res.cloudinary.com/debhhnzgh/image/upload/v1766658057/97972f54-6319-4b6e-a51c-8598ad558288.png"
       />
     </div>
   );
