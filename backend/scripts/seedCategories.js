5a58bd090b5403035419205eaa547c98959759d8
Rishikesh5577 <rishiwakchaure2003@gmail.com>
2025-12-28 18:21:48 +0530

    Update scripts and frontend components

--- backend/scripts/seedCategories.js
+++ backend/scripts/seedCategories.js
@@ -191,3 +191,4 @@ seedCategories();
 
 
 
+
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -10,22 +10,22 @@ const Home = () => {
            {
             desktop:
               "https://res.cloudinary.com/debhhnzgh/image/upload/v1766486888/Artboard_1_4x-100_ns3ryn.jpg",
-            alt: "Festive Offer - TickNTrack",
+            alt: "RestroBanner1",
           },
           {
             desktop:
               "https://res.cloudinary.com/debhhnzgh/image/upload/v1765977603/Red_And_Orange_Elegant_Collection_Launch_Banner_1920_x_600_mm_3_hlv0eh.svg",
-            alt: "TickNTrack - Premium Shoes & Watches Collection",
+            alt: "Restrobanner2",
           },
           {
             desktop:
               "https://res.cloudinary.com/debhhnzgh/image/upload/v1766658673/Brown_and_Beige_Scrapbook_Collage_India_Video_1920_x_600_px_6_tgp3ti.png",
-            alt: "TickNTrack - Premium Collection",
+            alt: "Restrobanner3",
           },
         
          
         ]}
-        mobileSrc="https://res.cloudinary.com/debhhnzgh/image/upload/v1766658057/97972f54-6319-4b6e-a51c-8598ad558288.png"
+        mobileSrc="https://res.cloudinary.com/debhhnzgh/image/upload/v1766925734/IMG_20251228_181115_c6o3io.png"
       />
     </div>
   );
